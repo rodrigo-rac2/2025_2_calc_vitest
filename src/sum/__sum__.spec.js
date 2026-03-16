986e1ede97b23d2a137045daa7ded7d7e2b021f1
rodrigo-rac2 <rodrigo.costa@gmail.com>
2026-03-16 10:01:47 -0300

    calc v1

---
+++ src/sum/__sum__.spec.js
@@ -0,0 +1,30 @@
+import { expect, test } from 'vitest'
+import { sum, sumList } from './sum.js'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
+
+test('adds 10000 + -2 to equal 3', () => {
+    expect(sum(10000, -2)).toBe(9998)
+  })
+
+test('adds 0 + 0 to equal 0', () => {
+    expect(sum(0, 0)).toBe(0)
+  })
+
+test('adds until the INT limit in JS', () => {
+    expect(sum(Number.MAX_SAFE_INTEGER, 1)).toBe(Number.MAX_SAFE_INTEGER + 1)
+  })
+
+test('adds until the INT limit negative in JS', () => {
+    expect(sum(-Number.MAX_SAFE_INTEGER, -1)).toBe(-Number.MAX_SAFE_INTEGER - 1)
+  })
+
+test('sums a list of numbers', () => {
+    expect(sumList([1, 2, 3, 4, 5])).toBe(15)
+  })
+
+test('sums a list of numbers - negative test', () => {
+    expect(sumList([1, 2, 3, 4, 5])).not.toBe(10)
+  })
---
+++ src/sum/sum.js
@@ -0,0 +1,10 @@
+export function sum(a, b) {
+    return a + b
+}
+
+// export function sumList(numbers) {
+//     return numbers.reduce(
+//         (sum, number) =>  
+//             sum + number, 
+//         0)
+// }
